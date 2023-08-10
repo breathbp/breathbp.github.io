85ff4dfb7d45bf82a7e933d69664cbc9e6d21bd0
Rahmouni Moulay <moulay.rahmouni1@gmail.com>
2023-08-10 16:54:17 +0100

    moulay

--- moulaySite/app.js
+++ moulaySite/app.js
@@ -24,3 +24,18 @@ const swiper = new Swiper('.swiper', {
   AOS.init({
     duration: 1200,
   });
+
+  document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 100) {
+          document.getElementById('navbar_top').classList.add('fixed-top');
+          // add padding top to show content behind navbar
+          navbar_height = document.querySelector('.navbar').offsetHeight;
+          document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+          document.getElementById('navbar_top').classList.remove('fixed-top');
+           // remove padding top from body
+          document.body.style.paddingTop = '0';
+        } 
+    });
+  }); 
