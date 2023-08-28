11c463997adafacc2fbc1e607cbcbf3e0820fbbd
boumediene-med <boumediene.bp2023@gmail.com>
2023-08-28 13:42:30 +0100

    update

--- moulaySite/admin/assets/js/reservation.js
+++ moulaySite/admin/assets/js/reservation.js
@@ -1,22 +1,4 @@
 $(document).ready(function () {
-$(document).on('click','.delete-reservation',function(){
-    const reservationCode = this.dataset.reservationCode;
-    const uri = "";
-    const dataObject ={resevation_code: reservationCode};
-    $.ajax({
-        url: uri,
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        method: 'POST',
-        data: JSON.stringify(dataObject),
-        success: function (response) {
-            // const responseData = response.json();
-            // const responseObject = JSON.parse(JSON.stringify(responseData));
-            // formElement.innerHTML = responseObject.message;
-        }
-    });
-});
 $(document).on('click','.deleteReservation',function(){
     const reservationCode = this.dataset.reservationCode;
     $('#deleteReservationCode').val(reservationCode);
@@ -44,7 +26,16 @@ $(document).on('click','#positiveButton',function(){
     
 });
 $(document).on('click','.close-modal',function(){
-    $('#deleteModal').modal('hide');
+    const target = this.dataset.bsTarget;
+    $(target).modal('hide');
+});
+
+//////SEND MESSAGE////////////////////
+// OPEN MODAL///////////////
+$(document).on('click','.sendMessage',function(){
+    const reservationCode = this.dataset.reservationCode;
+    $('#sendMsgReservationCode').val(reservationCode);
+    $('#sendMsgModal').modal('show');
 });
 
 });
