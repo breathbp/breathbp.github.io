c4ef569c72fcf95c830f5bfa8668c628648de0c8
boumediene-med <boumediene.bp2023@gmail.com>
2023-08-28 10:58:41 +0100

    update

--- moulaySite/admin/assets/js/reservation.js
+++ moulaySite/admin/assets/js/reservation.js
@@ -16,5 +16,35 @@ $(document).on('click','.delete-reservation',function(){
             // formElement.innerHTML = responseObject.message;
         }
     });
-})
+});
+$(document).on('click','.deleteReservation',function(){
+    const reservationCode = this.dataset.reservationCode;
+    $('#deleteReservationCode').val(reservationCode);
+    $('#deleteModal').modal('show');
+});
+$(document).on('click','#positiveButton',function(){
+    const reservation_code = $('#deleteReservationCode').val();
+    const uri = '';
+    const data = {resrvationCode : reservation_code};
+    $.post({
+        url : uri,
+        data: data,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function(){
+            //traitement
+            // $('#deleteModal').modal('hide');
+        },
+        error: function(){
+            // $('#deleteModal').modal('hide');
+        }
+    });
+    $('#deleteModal').modal('hide');
+    
+});
+$(document).on('click','.close-modal',function(){
+    $('#deleteModal').modal('hide');
+});
+
 });
