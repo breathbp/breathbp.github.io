18940fc5224ecae1eb8b8bc05b223fba7b464385
Mouley <moulay.rahmouni1@gmail.com>
2023-11-08 10:43:59 +0100

    vidéo inauguration evenement

--- moulaySite/assets/js/app.js
+++ moulaySite/assets/js/app.js
@@ -102,3 +102,31 @@ $(document).on('click', '.feature-tourisme-1n', function(e) {
     $("#ft2-responsive").attr('src', "images/place" + feature_id + ".jpg");
   }).fadeTo(300,1);
 });
+
+document.addEventListener("scroll", handleScroll);
+  
+  var scrollToTopBtn = document.querySelector(".back-to-top-button");
+
+  function handleScroll() {
+    var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var GOLDEN_RATIO = 0.5;
+  
+    if ((document.documentElement.scrollTop / scrollableHeight ) > GOLDEN_RATIO) {
+      //show button
+      if(!scrollToTopBtn.classList.contains("showScrollBtn"))
+      scrollToTopBtn.classList.add("showScrollBtn")
+    } else {
+      //hide button
+      if(scrollToTopBtn.classList.contains("showScrollBtn"))
+      scrollToTopBtn.classList.remove("showScrollBtn")
+    }
+  }
+  
+  scrollToTopBtn.addEventListener("click", scrollToTop);
+  
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }
